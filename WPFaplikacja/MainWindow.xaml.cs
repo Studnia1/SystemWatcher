bf55e8d39cdf65538e110fa3ff8c0f4b6630fe98
Studnia1 <49270452+Studnia1@users.noreply.github.com>
2020-08-06 17:21:56 +0200

    Add files via upload

---
+++ WPFaplikacja/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFaplikacja
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string sciezka;
+        ServiceReference1.WatcherServiceClient client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new ServiceReference1.WatcherServiceClient();
+
+
+        }
+
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    client.WypiszDaneDyskow();
+        //    lvUsers.ItemsSource = client.WypiszDaneDyskow();
+
+        //}
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            client.Run();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            client.Stop();
+            lvUsers.ItemsSource = client.PobierzListeZmian();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            lvUsers2.ItemsSource = client.WypiszDaneDyskow();
+        }
+
+        private void lvUsers2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sciezka = textbox1.Text;
+            client.GetPath(sciezka);
+        }
+
+        private void textbox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WatcherService/IWatcherService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WatcherService
+{
+    [ServiceContract]
+    public interface IWatcherService
+    {
+        [OperationContract]
+        void Run();
+        [OperationContract]
+        void Stop();
+        [OperationContract]
+        List<DaneZdyskow> WypiszDaneDyskow();
+        [OperationContract]
+        List<Dane> PobierzListeZmian();
+        [OperationContract]
+        void GetPath(string sciezka);
+    }
+
+    [DataContract]
+    public struct Dane
+    {
+        [DataMember]
+        public DateTime czas { get; set; }
+        [DataMember]
+        public string zmiana { get; set; }
+    }
+    [DataContract]
+    public struct DaneZdyskow
+    {
+        [DataMember]
+        public string a { get; set; }
+        [DataMember]
+        public string b { get; set; }
+        [DataMember]
+        public string c { get; set; }
+        [DataMember]
+        public string d { get; set; }
+        [DataMember]
+        public string e { get; set; }
+        [DataMember]
+        public string f { get; set; }
+        [DataMember]
+        public string g { get; set; }
+
+    }
+
+}
